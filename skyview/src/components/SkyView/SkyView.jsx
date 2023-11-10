ad408fbeca1e73ae099fe89e9e1155681def29ea
Ted Pieplow <ted.pieplow@gmail.com>
2023-11-10 17:52:32 +0100

    Added conditionfunction

--- skyview/src/components/SkyView/SkyView.jsx
+++ skyview/src/components/SkyView/SkyView.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from 'react'
-
-import './SkyView.css'
-
-
+import React, { useState, useEffect } from 'react';
+import './SkyView.css';
 
 const Skyview = () => {
     let api_key = 'dec8c5f710ee43b8be2205905230611';
@@ -10,51 +7,59 @@ const Skyview = () => {
     const [city, setCity] = useState('');
     const [temperature, setTemperature] = useState(null);
     const [wind, setWind] = useState(null);
-    const [location, setLocation] = useState(null);
-    const [region, setRegion] = useState(null);
-    const [country, setCountry] = useState(null);
 
     useEffect(() => {
-        getWeather('Lund')
+        getWeather('Blentarp');
     }, []);
 
     useEffect(() => {
         if (weather && weather.current) {
-           const { temp_c, location, country, region, wind } = weather.current;
+            const { temp_c, location, country, region, wind } = weather.current;
 
-           if (temp_c) {
-            setTemperature(temp_c);
-           }
-
-           if (wind) {
-            setWind(wind)
-           }
+            if (temp_c) {
+                setTemperature(temp_c);
+            }
         }
-    }, [weather])
+    }, [weather]);
 
     const getWeather = async (query) => {
         try {
             const result = await fetch(`http://api.weatherapi.com/v1/current.json?key=${api_key}&q=${query}`);
             const weatherData = await result.json();
-            setWeather(weatherData)
+            setWeather(weatherData);
             console.log(weatherData)
         } catch (error) {
             console.error('Error fetching weather', error);
         }
-    }
+    };
 
-    const dayOrNight = () => {
-        if (is_day === 0) {
-
+    const search = () => {
+        if (city !== '') {
+            getWeather(city);
         } else {
-            
+            console.log('City name is required for search');
+        }
+    };
+
+    const getCurrentCondition = (isDay, conditionCode) => {
+        const imageSrc = conditionCode && conditionCode.icon ? conditionCode.icon : isDay ? 'sun' : 'moon';
+
+        return <img src={imageSrc} alt={conditionCode?.text || (isDay ? 'sun' : 'moon')} />;
+    };
+
+    const displayMessage = () => {
+        if (weather && weather.current.condition) {
+            if (current.condition.test.includes('rain')) {
+                return "You better bring an umbrella with you"
+            } 
         }
     }
 
     const getBackgroundSwitch = () => {
-        if (weather) {
+        if (weather && weather.current) {
+            const condition = weather.current.condition.text.toLowerCase();
 
-            switch (weather) {
+            switch (condition) {
                 case 'rainy':
                     return 'rainy-background';
                 case 'sunny':
@@ -68,15 +73,6 @@ const Skyview = () => {
             }
         }
         return 'default-background';
-    };
-
-
-    const search = () => {
-        if (city !== '') {
-            getWeather(city);
-        } else {
-            console.log('City name is required for search');
-        }
     };
 
     return (
@@ -95,37 +91,36 @@ const Skyview = () => {
                     </div>
                 </div>
                 <div className="weather-image">
-                    <img src="" alt="" />
+                    {getCurrentCondition(weather?.current?.is_day, weather?.current?.condition)}
+                    <div className="weather-temp"> {temperature ? `${temperature}°C` : 'N/A'} </div>
+                    <div className="weather-temp">Feels like {temperature ? weather.current.feelslike_c : 'N/A'}</div>
+                    <div className="weather-location"> {weather ? weather.location.name : 'N/A'} </div>
                 </div>
                 {weather ? (
                     <div key={api_key}>
-                        <p>Location: {weather.location.name}</p>
-                        <p>Region: {weather.location.region}</p>
-                        <p>Country: {weather.location.country}</p>
-                        <p>Updated: {weather.current.last_updated}</p>
+                        <p>{weather.current.condition.text}</p>
                     </div>
                 ) : (
                     <p>No weather found</p>
-                )
-                }
-                <div className="weather-temp"> {temperature ? `${temperature}°C` : 'N/A'} </div>
-                <div className="weather-location"> {weather ? weather.location.name : 'N/A'} </div>
+                )}
                 <div className="wind-speed">{weather && weather.current.wind_kph ? `${((weather.current.wind_kph * 1000) / 3600).toFixed(1)} m/s` : 'N/A'}</div>
                 <div className="data-container">
                     <div className="element">
                         <img src="" alt="" />
                         <div className="data">
-                            <div className="humidity-percent">64%</div>
+                            <div className="humidity-percent">{weather ? `${weather.current.humidity}%` : 'N/A'}</div>
                             <div className="text">Humidity</div>
                         </div>
                     </div>
                 </div>
             </div>
-                Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a>
+            Powered by <a href="https://www.weatherapi.com/" title="Weather API">WeatherAPI.com</a>
+            <a href="">7 days forecast</a>
         </section>
-    )
-}
-
-
+    );
+};
 
 export default Skyview;
+
+
+
