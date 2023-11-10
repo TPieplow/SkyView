0277a164e5fd67a033a63812e9cdeec4125ca844
Ted Pieplow <ted.pieplow@gmail.com>
2023-11-10 20:39:49 +0100

    Added optional chaining to object nesting in API

--- skyview/src/components/SkyView/SkyView.jsx
+++ skyview/src/components/SkyView/SkyView.jsx
@@ -6,14 +6,13 @@ const Skyview = () => {
     const [weather, setWeather] = useState(null);
     const [city, setCity] = useState('');
     const [temperature, setTemperature] = useState(null);
-    const [wind, setWind] = useState(null);
 
     useEffect(() => {
         getWeather('Blentarp');
     }, []);
 
     useEffect(() => {
-        if (weather && weather.current) {
+        if (weather?.current) {
             const { temp_c, location, country, region, wind } = weather.current;
 
             if (temp_c) {
@@ -42,23 +41,35 @@ const Skyview = () => {
     };
 
     const getCurrentCondition = (isDay, conditionCode) => {
-        const imageSrc = conditionCode && conditionCode.icon ? conditionCode.icon : isDay ? 'sun' : 'moon';
+        const imageSrc = conditionCode?.icon ? conditionCode?.icon : isDay ? 'sun' : 'moon';
 
         return <img src={imageSrc} alt={conditionCode?.text || (isDay ? 'sun' : 'moon')} />;
     };
 
-    const displayMessage = () => {
-        if (weather && weather.current.condition) {
-            if (current.condition.test.includes('rain')) {
-                return "You better bring an umbrella with you"
+    const weatherMessages = {
+        rain: "You better bring an umbrella with you",
+        sunny: "Enjoy the weather",
+        snow: "Get ready for a snowy day",
+        overcast: "Depressing, isnt it - get yourself som chocolate"
+    }
+
+    const displayMessage = (weather) => {
+        if (weather?.current?.condition) {
+            const conditionText = weather?.current?.condition?.text.toLowerCase();
+            if (weatherMessages[conditionText]) {
+                return weatherMessages[conditionText]
+            } else {
+                return `${conditionText}`
             } 
+        } else {
+            return "Weather information is not available"
         }
     }
 
-    const getBackgroundSwitch = () => {
-        if (weather && weather.current) {
-            const condition = weather.current.condition.text.toLowerCase();
-
+    const getBackgroundSwitch = (weather) => {
+        if (weather?.current) {
+            const condition = weather?.current?.condition?.text.toLowerCase();
+            
             switch (condition) {
                 case 'rainy':
                     return 'rainy-background';
@@ -68,23 +79,25 @@ const Skyview = () => {
                     return 'snowy-background';
                 case 'windy':
                     return 'windy-background';
+                case 'overcast':
+                    return 'overcast-background'
                 default:
                     return 'default-background';
             }
         }
         return 'default-background';
     };
-
+    console.log(getBackgroundSwitch(weather))
     return (
-        <section className={`app-container ${getBackgroundSwitch()}`}>
-            <div className='app-container container'>
+        <section className={`app-container ${getBackgroundSwitch(weather)}`}>
+            <div className='container'>
                 <div className='top-bar'>
                     <input
                         type="text"
                         className='cityInput'
                         placeholder='Search ...'
                         value={city}
-                        onChange={(e) => setCity(e.target.value)}
+                        onChange={(e) => setCity(e?.target?.value)}
                     />
                     <div className='search-icon' onClick={search}>
                         <i className="fa-sharp fa-light fa-magnifying-glass-location"></i>
@@ -92,23 +105,24 @@ const Skyview = () => {
                 </div>
                 <div className="weather-image">
                     {getCurrentCondition(weather?.current?.is_day, weather?.current?.condition)}
+                    {displayMessage(weather)}
                     <div className="weather-temp"> {temperature ? `${temperature}°C` : 'N/A'} </div>
-                    <div className="weather-temp">Feels like {temperature ? weather.current.feelslike_c : 'N/A'}</div>
-                    <div className="weather-location"> {weather ? weather.location.name : 'N/A'} </div>
+                    <div className="weather-temp">Feels like {temperature ? weather?.current?.feelslike_c : 'N/A'}</div>
+                    <div className="weather-location"> {weather ? weather?.location?.name : 'N/A'} </div>
                 </div>
                 {weather ? (
                     <div key={api_key}>
-                        <p>{weather.current.condition.text}</p>
+                        <p>{weather?.current?.condition?.text}</p>
                     </div>
                 ) : (
                     <p>No weather found</p>
                 )}
-                <div className="wind-speed">{weather && weather.current.wind_kph ? `${((weather.current.wind_kph * 1000) / 3600).toFixed(1)} m/s` : 'N/A'}</div>
+                <div className="wind-speed">{weather?.current.wind_kph ? `${((weather?.current?.wind_kph * 1000) / 3600).toFixed(1)} m/s` : 'N/A'}</div>
                 <div className="data-container">
                     <div className="element">
                         <img src="" alt="" />
                         <div className="data">
-                            <div className="humidity-percent">{weather ? `${weather.current.humidity}%` : 'N/A'}</div>
+                            <div className="humidity-percent">{weather ? `${weather?.current?.humidity}%` : 'N/A'}</div>
                             <div className="text">Humidity</div>
                         </div>
                     </div>
