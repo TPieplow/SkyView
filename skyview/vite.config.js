f0ee8d0569dacfc337669abee0af18e023cb5edb
Ted Pieplow <ted.pieplow@gmail.com>
2023-11-15 21:55:46 +0100

    GH Pages

--- skyview/vite.config.js
+++ skyview/vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/skyview"
+  base: "/skyview/",
+  build: {
+    outDir: 'public',
+  }
 })
